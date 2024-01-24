56f0b8ad81e005ec426f121d298d740160a4f1dc
sarzig <63985693+sarzig@users.noreply.github.com>
2024-01-24 16:44:59 -0500

    reworked labs directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -17,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
