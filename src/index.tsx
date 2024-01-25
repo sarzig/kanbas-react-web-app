93df3c1e0c4410cfa135877d9b8b9803e439bc77
sarzig <63985693+sarzig@users.noreply.github.com>
2024-01-24 23:34:00 -0500

    added course nav

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -17,4 +17,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-*/
