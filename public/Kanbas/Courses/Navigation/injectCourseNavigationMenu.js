2194415b19d95873fc2d31a9ad4faad43c34283d
sarzig <63985693+sarzig@users.noreply.github.com>
2024-02-09 12:50:04 -0500

    course home screen successfully injected js

---
+++ public/Kanbas/Courses/Navigation/injectCourseNavigationMenu.js
@@ -0,0 +1,32 @@
+// injectCourseNavigationMenu.js
+
+// Wrap your HTML content in a function
+function injectCourseNavigationMenu() {
+    var html = `
+    <ul class="wd-navigation avoid-kanbas-nav-column-tight">
+    <li class="wd-active"><a href="/Kanbas/Courses/Home/screen.html">Home</a></li>
+    <li><a href="#">Modules</a></li>
+    <li><a href="#">Piazza</a></li>
+    <li><a href="/Kanbas/Courses/Grades/screen.html">Grades</a></li>
+    <li><a href="/Kanbas/Courses/Assignments/screen.html">Assignments</a></li>
+    <li><a href="#">Quizzes</a></li>
+    <li><a href="#">Zoom Meetings</a></li>
+    <li><a href="#">People</a></li>
+    <li><a href="#">Panopto Video</a></li>
+    <li><a href="#">Discussions</a></li>
+    <li><a href="#">Announcements</a></li>
+    <li><a href="#">Pages</a></li>
+    <li><a href="#">Files</a></li>
+    <li><a href="#">Rubrics</a></li>
+    <li><a href="#">Outcomes</a></li>
+    <li><a href="#">Collaborations</a></li>
+    <li><a href="#">Syllabus</a></li>
+    <li><a href="/Kanbas/Courses/Settings/screen.html">Settings</a></li>
+</ul>`;
+
+    // Append the HTML content directly to the body of the document
+    document.body.insertAdjacentHTML('afterbegin', html);
+}
+
+// Call the function to inject the fixed menu
+injectCourseNavigationMenu();
--- public/Kanbas/Navigation/injectMainNavigationMenu.js
+++ public/Kanbas/Navigation/injectMainNavigationMenu.js
@@ -2,20 +2,25 @@
 
 // Wrap your HTML content in a function
 function injectMainNavigationMenu() {
+    // Extract the activeIndex from the query parameters in the URL
+    const scriptSrc = document.currentScript.src;
+    const url = new URL(scriptSrc);
+    const activeIndex = url.searchParams.get('activeIndex') || '0';
+
     var html = `
         <div class="d-none d-sm-block kanbas-nav-column">
             <ul class="wd-kanbas-navigation">
                 <li><a href="http://northeastern.edu"><i class="custom-northeastern-icon"></i></a></li>
                 <li><a href="/Kanbas/Account/Profile/screen.html"><i class="fa fa-smile-o"></i><br>Account</a></li>
-                <li class="wd-active"><a href="/Kanbas/Dashboard/screen.html"><i class="fa fa-tachometer"></i><br>Dashboard</a></li>
-                <li><a href="/Kanbas/Courses/Home/screen.html"><i class="fa fa-book"></i><br>Courses</a></li>
-                <li><a href="#"><i class="fa fa-calendar"></i><br>Calendar</a></li>
-                <li><a href="#"><i class="fa fa-inbox"></i><br>Inbox</a></li>
-                <li><a href="#"><i class="fa fa-clock-o"></i><br>History</a></li>
-                <li><a href="#"><i class="fa fa-play"></i><br>Studio</a></li>
-                <li><a href="#"><i class="fa fa-calendar"></i><br>Commons</a></li>
-                <li><a href="#"><i class="fa fa-question"></i><br>Help</a></li>
-                <li><a href="#"><i class="fa fa-envelope"></i><br>Messages</a></li>
+                <li ${activeIndex === 'dashboard' ? 'class="wd-active"' : ''}><a href="/Kanbas/Dashboard/screen.html"><i class="fa fa-tachometer"></i><br>Dashboard</a></li>
+                <li ${activeIndex === 'courses' ? 'class="wd-active"' : ''}><a href="/Kanbas/Courses/Home/screen.html"><i class="fa fa-book"></i><br>Courses</a></li>
+                <li ${activeIndex === 'calendar' ? 'class="wd-active"' : ''}><a href="#"><i class="fa fa-calendar"></i><br>Calendar</a></li>
+                <li ${activeIndex === 'inbox' ? 'class="wd-active"' : ''}><a href="#"><i class="fa fa-inbox"></i><br>Inbox</a></li>
+                <li ${activeIndex === 'history' ? 'class="wd-active"' : ''}><a href="#"><i class="fa fa-clock-o"></i><br>History</a></li>
+                <li ${activeIndex === 'studio' ? 'class="wd-active"' : ''}><a href="#"><i class="fa fa-play"></i><br>Studio</a></li>
+                <li ${activeIndex === 'commons' ? 'class="wd-active"' : ''}><a href="#"><i class="fa fa-calendar"></i><br>Commons</a></li>
+                <li ${activeIndex === 'help' ? 'class="wd-active"' : ''}><a href="#"><i class="fa fa-question"></i><br>Help</a></li>
+                <li ${activeIndex === 'messages' ? 'class="wd-active"' : ''}><a href="#"><i class="fa fa-envelope"></i><br>Messages</a></li>
             </ul>
         </div>`;
 
