4ed0208ed0681a0bcc0532698af78f58b0f34267
sarzig <63985693+sarzig@users.noreply.github.com>
2024-02-09 16:39:29 -0500

    courses home (labs/a2.test) looking good but has no top bar

--- public/Kanbas/Navigation/injectMainNavigationMenu.js
+++ public/Kanbas/Navigation/injectMainNavigationMenu.js
@@ -1,8 +1,10 @@
+// injectMainNavigationMenu.js
+
 // Find the element with the id "inject-me"
-const targetElement = document.getElementById('inject-main-nav-sidebar');
+const targetElementMainNavigationMenu = document.getElementById('inject-main-nav-sidebar');
 
 // Check if the element exists before injecting the HTML
-if (targetElement) {
+if (targetElementMainNavigationMenu) {
     // Wrap your HTML content in a function
     function injectMainNavigationMenu() {
         // Extract the activeIndex from the query parameters in the URL
@@ -13,24 +15,24 @@ if (targetElement) {
         var html = `        
             <ul class="wd-kanbas-navigation">
                 <li><a href="http://northeastern.edu"><i class="custom-northeastern-icon"></i></a></li>
-                <li><a href="/Kanbas/Account/Profile/screen.html"><i class="fa fa-smile-o"></i><br>Account</a></li>
-                <li class="wd-active"><a href="/Kanbas/Dashboard/screen.html"><i class="fa fa-tachometer"></i><br>Dashboard</a></li>
-                <li><a href="/Kanbas/Courses/Home/screen.html"><i class="fa fa-book"></i><br>Courses</a> </li>
-                <li><a href="#"><i class="fa fa-calendar"></i><br>Calendar</a></li>
-                <li><a href="#"><i class="fa fa-inbox"></i><br>Inbox</a></li>
-                <li><a href="#"><i class="fa fa-clock-o"></i><br>History</a></li>
-                <li><a href="#"><i class="fa fa-play"></i><br>Studio</a></li>
-                <li><a href="#"><i class="fa fa-calendar"></i><br>Commons</a></li>
-                <li><a href="#"><i class="fa fa-question"></i><br>Help</a></li>
-                <li><a href="#"><i class="fa fa-envelope"></i><br>Messages</a></li>
+                <li${activeIndex === 'account' ? ' class="wd-active"' : ''}><a href="/Kanbas/Account/Profile/screen.html"><i class="fa fa-smile-o"></i><br>Account</a></li>
+                <li${activeIndex === 'dashboard' ? ' class="wd-active"' : ''}><a href="/Kanbas/Dashboard/screen.html"><i class="fa fa-tachometer"></i><br>Dashboard</a></li>
+                <li${activeIndex === 'courses' ? ' class="wd-active"' : ''}><a href="/Kanbas/Courses/Home/screen.html"><i class="fa fa-book"></i><br>Courses</a></li>
+                <li${activeIndex === 'calendar' ? ' class="wd-active"' : ''}><a href="#"><i class="fa fa-calendar"></i><br>Calendar</a></li>
+                <li${activeIndex === 'inbox' ? ' class="wd-active"' : ''}><a href="#"><i class="fa fa-inbox"></i><br>Inbox</a></li>
+                <li${activeIndex === 'history' ? ' class="wd-active"' : ''}><a href="#"><i class="fa fa-clock-o"></i><br>History</a></li>
+                <li${activeIndex === 'studio' ? ' class="wd-active"' : ''}><a href="#"><i class="fa fa-play"></i><br>Studio</a></li>
+                <li${activeIndex === 'commons' ? ' class="wd-active"' : ''}><a href="#"><i class="fa fa-calendar"></i><br>Commons</a></li>
+                <li${activeIndex === 'help' ? ' class="wd-active"' : ''}><a href="#"><i class="fa fa-question"></i><br>Help</a></li>
+                <li${activeIndex === 'messages' ? ' class="wd-active"' : ''}><a href="#"><i class="fa fa-envelope"></i><br>Messages</a></li>
             </ul>`;
-
         // Replace the innerHTML of the target element with the new HTML content
-        targetElement.innerHTML = html;
+        targetElementMainNavigationMenu.innerHTML = html;
     }
 
     // Call the function to inject the fixed menu
     injectMainNavigationMenu();
+
 } else {
-    console.error('Element with id "inject-me" not found.');
+    console.error('Element with id "inject-main-nav-sidebar" not found.');
 }
