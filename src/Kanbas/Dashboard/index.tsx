30e8c3b5fe01d6914d9e4ea32ab37b5c304b25a4
sarzig <63985693+sarzig@users.noreply.github.com>
2024-02-19 17:53:22 -0500

    dashboard looking beautiful

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { courses } from "../Database";
+import "./index.css";
 
 function Dashboard() {
   return (
@@ -8,26 +9,41 @@ function Dashboard() {
       <h1>Dashboard</h1>              <hr />
       <h2>Published Courses (12)</h2> <hr />
       <div className="row">
-        <div className="row row-cols-1 row-cols-md-5 g-4">
+        <div className="row row-cols-1 row-cols-md-5 g-200">
           {courses.map((course) => (
             <div key={course._id} className="col" style={{ width: 300 }}>
-              <div className="card">
-                <img src={`/images/${course.image}`} className="card-img-top"
-                     style={{ height: 150 }}/>
-                <div className="card-body">
-                  <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}
-                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                    {course.name} </Link>
-                  <p className="card-text">{course.name}</p>
-                  <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">
-                    Go </Link>
-                </div>
+              <div className="card flex-shrink-0">
+
+                <img src={`/images/${course.image}`} className="card-img-top" />
+
+                <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`} title={`${course.name} - Homepage`}>
+
+                  <div className="card-body">
+                    
+                    <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}>
+                      {course.name}
+                    </Link>
+
+                    <div className="card-code">
+                      {course.number}
+                    </div>
+
+                    <div className="card-details">
+                      {course.startDate} to {course.endDate}
+                    </div>
+
+                    <Link to={`/Kanbas/Courses/${course._id}/Assignments`} title={`${course.name} - Assignments`} className="btn btn-primary">
+                      Go
+                    </Link>
+                    
+                  </div>
+                </Link>
               </div>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 export default Dashboard;
--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -7,32 +7,34 @@ import { IoMdMail } from "react-icons/io";
 
 function KanbasNavigation() {
     const links = [
-        { label: "Account", icon: <FaRegUserCircle className="fs-2" /> },
-        { label: "Dashboard", icon: <FaTachometerAlt className="fs-2" /> },
-        { label: "Courses", icon: <FaBook className="fs-2" /> },
-        { label: "Calendar", icon: <FaRegCalendarAlt className="fs-2" /> },
-        { label: "Inbox", icon: <FaInbox className="fs-2" /> },
-        { label: "History", icon: <FaRegClock className="fs-2" /> },
-        { label: "Studio", icon: <FaPlay className="fs-2" /> },
-        { label: "Commons", icon: <FaRegUserCircle className="fs-2" /> },
-        { label: "Help", icon: <FaRegCircleQuestion className="fs-2" /> },
-        { label: "Messages", icon: <IoMdMail className="fs-2" /> },
+        { label: "Account", icon: <FaRegUserCircle className="fs-2 nav-icon" /> },
+        { label: "Dashboard", icon: <FaTachometerAlt className="fs-2 nav-icon" /> },
+        { label: "Courses", icon: <FaBook className="fs-2 nav-icon" /> },
+        { label: "Calendar", icon: <FaRegCalendarAlt className="fs-2 nav-icon" /> },
+        { label: "Inbox", icon: <FaInbox className="fs-2 nav-icon" /> },
+        { label: "History", icon: <FaRegClock className="fs-2 nav-icon" /> },
+        { label: "Studio", icon: <FaPlay className="fs-2 nav-icon" /> },
+        { label: "Commons", icon: <FaRegUserCircle className="fs-2 nav-icon" /> },
+        { label: "Help", icon: <FaRegCircleQuestion className="fs-2 nav-icon" /> },
+        { label: "Messages", icon: <IoMdMail className="fs-2 nav-icon" /> },
     ];
 
     const { pathname } = useLocation();
     return (
-        <ul className="wd-kanbas-navigation">
-            <li className="">
-                <a href="https://www.northeastern.edu/" target="_blank" rel="noopener noreferrer">
-                    <div className="custom-northeastern-icon" />
-                </a>
-            </li>
-            {links.map((link, index) => (
-                <li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
-                    <Link to={`/Kanbas/${link.label}`}> {link.icon}<br />{link.label} </Link>
+        <div className="col main-nav-sidebar">
+            <ul className="wd-kanbas-navigation">
+                <li className="">
+                    <a href="https://www.northeastern.edu/" target="_blank" rel="noopener noreferrer">
+                        <div className="custom-northeastern-icon" />
+                    </a>
                 </li>
-            ))}
-        </ul>
+                {links.map((link, index) => (
+                    <li key={index} className={pathname.includes(link.label) ? "wd-active" : ""}>
+                        <Link to={`/Kanbas/${link.label}`}> {link.icon}<br />{link.label} </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 export default KanbasNavigation;
