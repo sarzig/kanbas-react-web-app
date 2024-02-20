b2ffa3973d017079805fb27e6383ab0c8619fff3
sarzig <63985693+sarzig@users.noreply.github.com>
2024-02-20 09:08:28 -0500

    finished 3.4

---
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -0,0 +1,9 @@
+
+function Modules() {
+    return(
+        <>
+        <h2>HELLLOOOOOOOO</h2>
+        </>
+    );
+};
+export default Modules;
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -5,12 +5,16 @@ import CourseNavigation from "./Navigation";
 import React, { useState } from 'react';
 import "../styles.css";
 import "./index.css";
+import Modules from "./Modules";
+import {Link, useLocation} from "react-router-dom";
+
+/* todo: xxx implement routes as a loop */
 
 function Courses() {
   const { courseId } = useParams();
   const course = courses.find((course) => course._id === courseId);
 
-  const [isNavigationOpen, setNavigationOpen] = useState(false);
+  const [isNavigationOpen, setNavigationOpen] = useState(true);
 
   /* Handle the toggle feature */
   const handleToggleNavigation = () => {
@@ -18,33 +22,43 @@ function Courses() {
   };
 
   /* Handle the hover text when clicking hamburger */
-  const handleToggleTitle = () => {return isNavigationOpen ? "Hide Account Navigation Menu" : "Show Account Navigation Menu";};
+  const handleToggleTitle = () => { return isNavigationOpen ? "Hide Account Navigation Menu" : "Show Account Navigation Menu"; };
 
   return (
     <div className="main-content">
       <h1>
         <HiMiniBars3 className="top-bar-menu-hamburger" onClick={handleToggleNavigation} title={handleToggleTitle()} />
         Course {course?.name ?? 'Unknown Course'}
+
+        <Routes>
+          <Route path="/" element={<Navigate to="Home" />} />
+          <Route path="Home" element={<>{'  > '}Home</>} />
+          <Route path="Modules" element={<Link to="Modules">{'  > '}Modules</Link>} />
+          <Route path="Piazza" element={<>{'  > '}Piazza</>} />
+          <Route path="Grades" element={<>{'  > '}Grades</>} />
+          <Route path="Assignments" element={<>{'  > '}Assignments</>} />
+          <Route path="Assignments/:assignmentId" element={<>{'  > '}Assignment Editor</>} />
+          <Route path="Quizzes" element={<>{'  > '}Quizzes</>} />
+          <Route path="Zoom-Meetings" element={<>{'  > '}Zoom Meetings</>} />
+          <Route path="People" element={<>{'  > '}People</>} />
+          <Route path="Panopto-Video" element={<>{'  > '}Panopto Video</>} />
+          <Route path="Discussions" element={<>{'  > '}Discussions</>} />
+          <Route path="Announcements" element={<>{'  > '}Announcements</>} />
+          <Route path="Pages" element={<>{'  > '}Pages</>} />
+          <Route path="Files" element={<>{'  > '}Files</>} />
+          <Route path="Rubrics" element={<>{'  > '}Rubrics</>} />
+          <Route path="Outcomes" element={<>{'  > '}Outcomes</>} />
+          <Route path="Collaborations" element={<>{'  > '}Collaborations</>} />
+          <Route path="Syllabus" element={<>{'  > '}Syllabus</>} />
+          <Route path="Settings" element={<>{'  > '}Settings</>} />
+          <Route path="Modules" element={<Modules />} />
+        </Routes>
+        
       </h1>
       <hr />
 
       {isNavigationOpen && <CourseNavigation />}
 
-      <div>
-        <div
-          className="overflow-y-scroll position-fixed bottom-0 end-0"
-          style={{ left: "320px", top: "50px" }} >
-          <Routes>
-            <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<h1>Home</h1>} />
-            <Route path="Modules" element={<h1>Modules</h1>} />
-            <Route path="Piazza" element={<h1>Piazza</h1>} />
-            <Route path="Assignments" element={<h1>Assignments</h1>} />
-            <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
-            <Route path="Grades" element={<h1>Grades</h1>} />
-          </Routes>
-        </div>
-      </div>
     </div>
   );
 };
