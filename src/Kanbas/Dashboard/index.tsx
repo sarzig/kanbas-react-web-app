67a848d17b9acbb49da8738aaa8cfc3f22d50dc9
sarzig <63985693+sarzig@users.noreply.github.com>
2024-03-29 12:30:31 -0400

    deleted public folder and broke images

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -23,7 +23,8 @@ function Dashboard() {
 
   return (
     <div>
-      <h1>Dashboard</h1>              <hr />
+      <h1>Dashboard</h1>              
+      <hr />
       <h5>Course</h5>
       <input value={course.name} className="form-control"
         onChange={(e) => setCourse({ ...course, name: e.target.value })} />
