37f9c97b668e9b35766dd2c29b8d404d528adec6
sarzig <63985693+sarzig@users.noreply.github.com>
2024-04-07 14:55:03 -0400

    added link to 4001 node

--- src/Labs/A5/index.tsx
+++ src/Labs/A5/index.tsx
@@ -1,16 +1,12 @@
-import React from "react";
-import ClickEvent from "../A4/ClickEvent";
-
 function Assignment5() {
-
     return (
-        <div>
-            <h1>Assignment 5</h1>
-            <ClickEvent />
-        
-        
-        
-        </div>
+      <div>
+        <h1>Assignment 5</h1>
+        <a href="http://localhost:4001/a5/welcome">
+          Welcome
+        </a>
+      </div>
     );
-};
-export default Assignment5;
+  }
+  export default Assignment5;
+  
