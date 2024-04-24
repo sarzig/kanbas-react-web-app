b5639ef99a7f4ead770fe8be387d3cec87c20710
Sarah <63985693+sarzig@users.noreply.github.com>
2024-04-24 11:24:05 -0400

    make a5 default

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -13,7 +13,7 @@ function Labs() {
                 <Nav />
                 <div style={{ margin: '20px' }}>
                     <Routes>
-                        <Route path="/" element={<Navigate to="a4" />} />
+                        <Route path="/" element={<Navigate to="a5" />} />
                         <Route path="a3" element={<Assignment3 />} />
                         <Route path="a4" element={<Assignment4 />} />
                         <Route path="a5" element={<Assignment5 />} />
